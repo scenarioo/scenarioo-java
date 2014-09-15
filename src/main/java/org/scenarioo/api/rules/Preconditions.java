bcb3e9c06ff31a2f30492c3edffd2e2b5c853a1f
adiherzog <adrian.herzog@gmail.com>
2014-09-15 09:34:47 +0200

    #336 Changed exception to NullPointerException to conform with Guava.

--- src/main/java/org/scenarioo/api/rules/Preconditions.java
+++ src/main/java/org/scenarioo/api/rules/Preconditions.java
@@ -10,7 +10,7 @@ public class Preconditions {
 	
 	public static void checkNotNull(final Object object, final String message) {
 		if (object == null) {
-			throw new IllegalArgumentException(message);
+			throw new NullPointerException(message);
 		}
 	}
 	
