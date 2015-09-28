0c817ca0bca2317b2493048d915239e89a115865
bruderol <bruderer@computerscience.ch>
2015-09-28 15:51:26 +0200

    scenarioo/scenarioo#149 changed values for enums for screen annotations for upper case for consistency of webapp with server

--- src/main/java/org/scenarioo/model/docu/entities/ScreenAnnotationClickAction.java
+++ src/main/java/org/scenarioo/model/docu/entities/ScreenAnnotationClickAction.java
@@ -1,13 +1,11 @@
 package org.scenarioo.model.docu.entities;
 
-import javax.xml.bind.annotation.XmlEnumValue;
 
 public enum ScreenAnnotationClickAction {
 
 	/**
 	 * Let the user navigate to the next step when he clicks on the annotation
 	 */
-	@XmlEnumValue(value = "toNextStep")
 	TO_NEXT_STEP,
 
 	/**
@@ -15,7 +13,6 @@ public enum ScreenAnnotationClickAction {
 	 * 
 	 * The URL will be opened in a seperate browser tab.
 	 */
-	@XmlEnumValue(value = "toUrl")
 	TO_URL;
 
 }
