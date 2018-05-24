1998ab6992bafef55d4729135efed5036b2a4819
Rolf Bruderer <bruderer@computerscience.ch>
2018-05-24 23:38:31 +0200

    #14 Fix loading of builds and branches to not fail just because one build or branch is somehow corrupt, ignore it in this case and log an error.

--- src/main/java/org/scenarioo/api/ScenarioDocuReader.java
+++ src/main/java/org/scenarioo/api/ScenarioDocuReader.java
@@ -1,21 +1,21 @@
 /* scenarioo-api
  * Copyright (C) 2014, scenarioo.org Development Team
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * As a special exception, the copyright holders of this library give you 
- * permission to link this library with independent modules, according 
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules, according
  * to the GNU General Public License with "Classpath" exception as provided
  * in the LICENSE file that accompanied this code.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -40,28 +40,28 @@ import org.scenarioo.model.docu.entities.UseCase;
  * Gives access to the geenrated scenario docu files in the filesystem.
  */
 public class ScenarioDocuReader {
-	
+
 	private final ScenarioDocuFiles docuFiles;
-	
+
 	public ScenarioDocuReader(final File rootDirectory) {
 		this.docuFiles = new ScenarioDocuFiles(rootDirectory);
 	}
-	
+
 	public Branch loadBranch(final String branchName) {
 		File file = docuFiles.getBranchFile(checkIdentifier(branchName));
 		return ScenarioDocuXMLFileUtil.unmarshal(Branch.class, file);
 	}
-	
+
 	public List<Branch> loadBranches() {
 		List<File> branchFiles = docuFiles.getBranchFiles();
-		return ScenarioDocuXMLFileUtil.unmarshalListOfFiles(Branch.class, branchFiles);
+		return ScenarioDocuXMLFileUtil.unmarshalListOfFilesIgnoreInvalid(Branch.class, branchFiles);
 	}
-	
+
 	public Build loadBuild(final String branchName, final String buildName) {
 		File file = docuFiles.getBuildFile(checkIdentifier(branchName), checkIdentifier(buildName));
 		return ScenarioDocuXMLFileUtil.unmarshal(Build.class, file);
 	}
-	
+
 	/**
 	 * This method was changed in in Version 2.0 of the API to return a list of ObjectFromDirectory&lt;Build&gt; instead
 	 * of a list of BuildLinks. This was done because BuildLinks belongs to the Server now and is not part of the API
@@ -69,47 +69,47 @@ public class ScenarioDocuReader {
 	 */
 	public List<ObjectFromDirectory<Build>> loadBuilds(final String branchName) {
 		List<File> buildFiles = docuFiles.getBuildFiles(checkIdentifier(branchName));
-		return ScenarioDocuXMLFileUtil.unmarshalListOfFilesWithDirNames(buildFiles, Build.class);
+		return ScenarioDocuXMLFileUtil.unmarshalListOfFilesWithDirNamesIgnoreInvalid(buildFiles, Build.class);
 	}
-	
+
 	public List<UseCase> loadUsecases(final String branchName, final String buildName) {
 		List<File> files = docuFiles.getUseCaseFiles(checkIdentifier(branchName), checkIdentifier(buildName));
 		return ScenarioDocuXMLFileUtil.unmarshalListOfFiles(UseCase.class, files);
 	}
-	
+
 	public UseCase loadUsecase(final String branchName, final String buildName, final String useCaseName) {
 		File file = docuFiles.getUseCaseFile(checkIdentifier(branchName), checkIdentifier(buildName),
 				checkIdentifier(useCaseName));
 		return ScenarioDocuXMLFileUtil.unmarshal(UseCase.class, file);
 	}
-	
+
 	public List<Scenario> loadScenarios(final String branchName, final String buildName, final String useCaseName) {
 		List<File> files = docuFiles.getScenarioFiles(checkIdentifier(branchName), checkIdentifier(buildName),
 				checkIdentifier(useCaseName));
 		return ScenarioDocuXMLFileUtil.unmarshalListOfFiles(Scenario.class, files);
 	}
-	
+
 	public Scenario loadScenario(final String branchName, final String buildName, final String useCaseName,
 			final String scenarioName) {
 		File file = docuFiles.getScenarioFile(checkIdentifier(branchName), checkIdentifier(buildName),
 				checkIdentifier(useCaseName), checkIdentifier(scenarioName));
 		return ScenarioDocuXMLFileUtil.unmarshal(Scenario.class, file);
 	}
-	
+
 	public List<Step> loadSteps(final String branchName, final String buildName, final String useCaseName,
 			final String scenarioName) {
 		List<File> files = docuFiles.getStepFiles(checkIdentifier(branchName), checkIdentifier(buildName),
 				checkIdentifier(useCaseName), checkIdentifier(scenarioName));
 		return ScenarioDocuXMLFileUtil.unmarshalListOfFiles(Step.class, files);
 	}
-	
+
 	public Step loadStep(final String branchName, final String buildName, final String useCaseName,
 			final String scenarioName, final int stepIndex) {
 		File file = docuFiles.getStepFile(checkIdentifier(branchName), checkIdentifier(buildName),
 				checkIdentifier(useCaseName), checkIdentifier(scenarioName), stepIndex);
 		return ScenarioDocuXMLFileUtil.unmarshal(Step.class, file);
 	}
-	
+
 	/**
 	 * Screenshot files are simply provided by path, the REST service will take care of streaming it.
 	 */
@@ -118,5 +118,5 @@ public class ScenarioDocuReader {
 		return new File(docuFiles.getScreenshotsDirectory(checkIdentifier(branchName), checkIdentifier(buildName),
 				checkIdentifier(useCaseName), checkIdentifier(scenarioName)), imageName);
 	}
-	
+
 }
